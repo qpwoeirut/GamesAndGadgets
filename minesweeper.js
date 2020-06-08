4e40de53600ee614f0356f2ac584b4460a16a392
qpwoeirut <stanleyzhong8@gmail.com>
2020-06-08 15:42:09 -0700

    add popup to edit settings

--- minesweeper.js
+++ minesweeper.js
@@ -12,7 +12,7 @@ const UNINITIALIZED = 11;
 const SCOREBOARD_SIZE = 3;
 
 const DEFAULT_ROW = 16;
-const DEFAULT_COL = 30;
+const DEFAULT_COLS = 30;
 const DEFAULT_MINES = 99;
 
 var BYPASS_SIZE_CHECK = false;
@@ -31,73 +31,9 @@ const chCol = [1, 0, -1, 1, 1, 0, -1, -1];
 
 function startGame(game) {
     console.debug("invoked startGame with game")
-    const rowInput = document.getElementById("rowCount");
-    const colInput = document.getElementById("colCount");
-    const mineInput = document.getElementById("mineCount");
-    game.rows = parseInt(rowInput.value);
-    game.cols = parseInt(colInput.value);
-    game.mineCount = parseInt(mineInput.value);
-
-    if (isNaN(game.rows)) {
-        game.rows = DEFAULT_ROW;
-        rowInput.value = DEFAULT_ROW;
-        console.warn("Input for number of rows is invalid. Defaulting to " + DEFAULT_ROW + " rows");
-        alert("Input for number of rows is invalid. Defaulting to " + DEFAULT_ROW + " rows");
-    }
-    if (isNaN(game.cols)) {
-        game.cols = DEFAULT_COL;
-        colInput.value = DEFAULT_COL;
-        console.warn("Input for number of columns is invalid. Defaulting to " + DEFAULT_COL + " columns");
-        alert("Input for number of columns is invalid. Defaulting to " + DEFAULT_COL + " columns");
-    }
-    if (isNaN(game.mineCount)) {
-        game.mineCount = DEFAULT_MINES;
-        mineInput.value = DEFAULT_MINES;
-        console.warn("Input for number of mines is invalid. Defaulting to " + DEFAULT_MINES + " mines");
-        alert("Input for number of mines is invalid. Defaulting to " + DEFAULT_MINES + " mines");
-    }
-
-    if (game.rows < MIN_SIZE && BYPASS_SIZE_CHECK !== true) {
-        console.warn("Grid must have at least " + MIN_SIZE + " rows");
-        console.warn(BYPASS_SIZE_CHECK_STRING);
-        alert("Grid must have at least " + MIN_SIZE + " rows. " + BYPASS_SIZE_CHECK_STRING);
-        game.rows = MIN_SIZE;
-    }
-    if (game.cols < MIN_SIZE && BYPASS_SIZE_CHECK !== true) {
-        console.warn("Grid must have at least " + MIN_SIZE + " columns");
-        console.warn(BYPASS_SIZE_CHECK_STRING);
-        alert("Grid must have at least " + MIN_SIZE + " columns. " + BYPASS_SIZE_CHECK_STRING);
-        game.cols = MIN_SIZE;
-    }
-    if (game.rows > MAX_SIZE && BYPASS_SIZE_CHECK !== true) {
-        console.warn("Grid cannot have over " + MAX_SIZE + " rows");
-        console.warn(BYPASS_SIZE_CHECK_STRING);
-        alert("Grid cannot have over " + MAX_SIZE + " rows. " + BYPASS_SIZE_CHECK_STRING);
-        game.rows = MAX_SIZE;
-    }
-    if (game.cols > MAX_SIZE && BYPASS_SIZE_CHECK !== true) {
-        console.warn("Grid cannot have over " + MAX_SIZE + " columns");
-        console.warn(BYPASS_SIZE_CHECK_STRING);
-        alert("Grid cannot have over " + MAX_SIZE + " columns. " + BYPASS_SIZE_CHECK_STRING);
-        game.cols = MAX_SIZE;
-    }
-    if ((game.rows * game.cols * MAX_MINE_PERCENTAGE / 100) <= game.mineCount && BYPASS_MINE_CHECK !== true) {
-        console.warn("In a " + game.rows + " by " + game.cols + " grid, " + game.mineCount + " mines is over " + MAX_MINE_PERCENTAGE + "% of the cells.");
-        console.warn(BYPASS_MINE_CHECK_STRING);
-        
-        const prevCount = game.mineCount;
-        game.mineCount = Math.floor(game.rows * game.cols * MAX_MINE_PERCENTAGE / 100);
-        console.warn("Defaulting to " + game.mineCount + " mines");
-        alert("In a " + game.rows + " by " + game.cols + " grid, " + prevCount + " mines is over " + MAX_MINE_PERCENTAGE + "% of the cells.\nDefaulting to " + game.mineCount + " mines.\n" + BYPASS_MINE_CHECK_STRING);
-    }
-    if (game.mineCount < 0) {
-        console.warn("Number of mines can't be negative");
-        const prevCount = game.mineCount;
-        game.mineCount = Math.max(1, Math.floor(game.rows * game.cols) / 20);
-        console.warn("Defaulting to " + game.mineCount + " mines");
-        alert("Number of mines can't be negative. Defaulting to " + game.mineCount + " mines");
-    }
-
+    game.rows = game.nextRows || DEFAULT_ROW;
+    game.cols = game.nextCols || DEFAULT_COLS;
+    game.mineCount = game.nextMineCount || DEFAULT_MINES;
     game.state = NOT_STARTED;
     game.grid = createGrid(UNINITIALIZED);
     game.status = createGrid(SECRET);
@@ -391,4 +327,80 @@ function cleanupGame() {
     document.getElementById("minesweeperContainer").classList.remove("active-game");
     clearInterval(game.timer);
     game.state = DONE;
+}
+
+
+function openPopup() {
+    document.getElementById("popupContainer").classList.add("active");
+}
+
+
+function closePopup(saveData) {
+    console.debug("invoking closePopup with saveData=" + saveData)
+    if (saveData === false) {
+        document.getElementById("rowCount").value = game.rows;
+        document.getElementById("colCount").value = game.cols;
+        document.getElementById("mineCount").value = game.mineCount;
+        document.getElementById("popupContainer").classList.remove("active");
+        return;
+    }
+    const rowInput = document.getElementById("rowCount").value;
+    const colInput = document.getElementById("colCount").value;
+    const mineInput = document.getElementById("mineCount").value;
+    if (isNaN(rowInput)) {
+        console.warn("Input for number of rows is invalid");
+        alert("Input for number of rows is invalid");
+        return;
+    }
+    if (isNaN(colInput)) {
+        console.warn("Input for number of columns is invalid. Defaulting to " + DEFAULT_COLS + " columns");
+        alert("Input for number of columns is invalid. Defaulting to " + DEFAULT_COLS + " columns");
+        return;
+    }
+    if (isNaN(mineInput)) {
+        console.warn("Input for number of mines is invalid. Defaulting to " + DEFAULT_MINES + " mines");
+        alert("Input for number of mines is invalid. Defaulting to " + DEFAULT_MINES + " mines");
+        return;
+    }
+
+    if (rowInput < MIN_SIZE && BYPASS_SIZE_CHECK !== true) {
+        console.warn("Grid must have at least " + MIN_SIZE + " rows");
+        console.warn(BYPASS_SIZE_CHECK_STRING);
+        alert("Grid must have at least " + MIN_SIZE + " rows.\n" + BYPASS_SIZE_CHECK_STRING);
+        return;
+    }
+    if (colInput < MIN_SIZE && BYPASS_SIZE_CHECK !== true) {
+        console.warn("Grid must have at least " + MIN_SIZE + " columns");
+        console.warn(BYPASS_SIZE_CHECK_STRING);
+        alert("Grid must have at least " + MIN_SIZE + " columns.\n" + BYPASS_SIZE_CHECK_STRING);
+        return;
+    }
+    if (rowInput > MAX_SIZE && BYPASS_SIZE_CHECK !== true) {
+        console.warn("Grid cannot have over " + MAX_SIZE + " rows");
+        console.warn(BYPASS_SIZE_CHECK_STRING);
+        alert("Grid cannot have over " + MAX_SIZE + " rows.\n" + BYPASS_SIZE_CHECK_STRING);
+        return;
+    }
+    if (colInput > MAX_SIZE && BYPASS_SIZE_CHECK !== true) {
+        console.warn("Grid cannot have over " + MAX_SIZE + " columns");
+        console.warn(BYPASS_SIZE_CHECK_STRING);
+        alert("Grid cannot have over " + MAX_SIZE + " columns.\n" + BYPASS_SIZE_CHECK_STRING);
+        return;
+    }
+    if ((rowInput * colInput * MAX_MINE_PERCENTAGE / 100) <= mineInput && BYPASS_MINE_CHECK !== true) {
+        console.warn("In a " + rowInput + " by " + colInput + " grid, " + mineInput + " mines is over " + MAX_MINE_PERCENTAGE + "% of the cells.");
+        console.warn(BYPASS_MINE_CHECK_STRING);
+        alert("In a " + rowInput + " by " + colInput + " grid, " + prevCount + " mines is over " + MAX_MINE_PERCENTAGE + "% of the cells\n" + BYPASS_MINE_CHECK_STRING);
+        return;
+    }
+    if (game.mineCount < 0) {
+        console.warn("Number of mines can't be negative");
+        alert("Number of mines can't be negative");
+        return;
+    }
+    game.nextRows = rowInput;
+    game.nextCols = colInput;
+    game.nextMineCount = mineInput;
+    document.getElementById("popupContainer").classList.remove("active");
+    console.debug("finished closePopup with game.nextRows=" + game.nextRows + ", game.nextCols=" + game.nextCols + ", game.nextMineCount=" + game.nextMineCount);
 }
