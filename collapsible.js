a0e3542315768fc6d80b9b1ec0bc5532516c56f7
qpwoeirut <stanleyzhong8@gmail.com>
2020-06-18 16:12:36 -0700

    change pattern lists to collapsibles

--- collapsible.js
+++ collapsible.js
@@ -1,6 +1,9 @@
 // If this doesn't seem to be working, make sure it's called AFTER the <body> tag
 function addCollapsibleListeners() {
     const controls = document.getElementsByClassName("collapsible-control");
+    if (controls.length === 0) {
+        console.warn("addCollapsibleListeners was called, but no collapsibles were found");
+    }
     for (const control of controls) {
         control.addEventListener("click", function (e) {
             this.classList.toggle("collapsible-open");
