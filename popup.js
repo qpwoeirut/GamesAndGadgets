75cb44dca0ececd229af18361c574db1b7fdf498
qpwoeirut <stanleyzhong8@gmail.com>
2020-07-23 21:58:39 -0700

    add How To Play for Pong

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 function closePopups(event) {
-    if (event.target.closest(".popup") === null && event.target.classList.contains("clickable") === false) {
+    if (event.target.closest(".popup") === null) {
         for (const popup of document.querySelectorAll(".popup:not(.no-click-away)")) {
             popup.classList.remove("active");
         }
@@ -8,12 +8,18 @@ function closePopups(event) {
 
 function openId(elemId) {
     logMessage("invoked openId with elemId=" + elemId);
-    document.getElementById(elemId).classList.add("active");
-
+    const elem = document.getElementById(elemId);
+    elem.classList.add("active");
+    if (elem.classList.contains("no-click-away")) {
+        return;
+    }
+    document.getElementById(elemId).classList.add("no-click-away");
+    setTimeout(function() {
+        document.getElementById(elemId).classList.remove("no-click-away")
+    }, 100);
 }
 
 function closeId(elemId) {
     logMessage("invoked closeId with elemId=" + elemId);
     document.getElementById(elemId).classList.remove("active");
 }
-
