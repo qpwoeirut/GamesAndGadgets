a81a00c509369156f44581557c85ea7bef4fc219
qpwoeirut <stanleyzhong8@gmail.com>
2021-07-15 00:34:39 -0700

    Revert "Merge branch 'master' of github.com:qpwoeirut/GamesAndGadgets"
    This reverts commit 1ba66104f3fc8422e3040290c3f6a31f9e7f31d6, reversing
    changes made to 82cc980026d9c85b76ebe4dbbeb03f50e87c9223.
    
    Stanley tries to undo the changes he made earlier and doesn't know how to use Git.

--- bird.js
+++ bird.js
@@ -8,108 +8,85 @@ var bird = {
     w: 35, 
     h: 25,
     speed: 0,
-    angle: 0
+    angle: 90
 };
-var ground = new Image();
-var groundInfo = {
-    x: 0,
-    y: 370
-}
 var floorPipe = new Image();
 var roofPipe = new Image();
-const pipeNum = 2 // Range: 1-3 (2)
 var pipeInfo = {
     gap: 100,
     constant: 0,
     h: 300,
-    w: 50,
-    dis: canvas.width / pipeNum,
-    num: pipeNum + 1,
-    speed: 2
+    w: 50
 };
 var environment = {
     score: 0,
-    play: false
 };
 
 var pipe = [];
 
-initPipes();
+pipe[0] = {
+    x: canvas.width,
+    y: 0
+};
 
 // Source images
 birdImg.src = "bird.png";  
-floorPipe.src = "floorPipe.png"
+floorPipe.src = "floorPipe.png";
 roofPipe.src = "roofPipe.png";
-ground.src = "ground.png";
 
 
 // Move the bird up when space is pressed
-function moveUp() {  
-    bird.speed = -4.8; // Range 3.8-5.8 (4.8)
+function moveUp(){  
+    bird.speed = -2.6; 
 } 
 
 function loop() {
-    if (environment.play === true) {
-        // Clear the canvas for redrawing
-        context.clearRect(0, 0, canvas.width, canvas.height);
+    // Clear the canvas for redrawing
+    context.clearRect(0, 0, canvas.width, canvas.height);
 
-        // Draw the bird
-        context.drawImage(birdImg, bird.x, bird.y);
+    // Draw the bird
+    // context.fillStyle = "purple"; ---> hitbox for debug
+    // context.fillRect(bird.x, bird.y, bird.w, bird.h);
+    context.drawImage(birdImg, bird.x - 87, bird.y - 73);
 
-        // Place pipe pairs
-        for (var i = 0; i < pipeInfo.num; i++) {
-            // Set the position of the gap in the pipes
-            pipeInfo.constant = pipeInfo.h + pipeInfo.gap;
-            // Draw the pipes
-            context.drawImage(roofPipe, pipe[i].x, pipe[i].y - 100);
-            context.drawImage(floorPipe, pipe[i].x, pipe[i].y + pipeInfo.constant);
+    // Place pipe pairs
+    for (var i = 0; i < pipe.length; i++) {
+        // Set the position of the gap in the pipes
+        pipeInfo.constant = pipeInfo.h + pipeInfo.gap;
+        // Draw the pipes
+        // context.fillStyle = "purple";
+        // context.fillRect(pipe[i].x, pipe[i].y, pipeInfo.w, pipeInfo.h);
+        context.drawImage(roofPipe, pipe[i].x, pipe[i].y - 100);
+        // context.fillStyle = "purple";
+        // context.fillRect(pipe[i].x, pipe[i].y + pipeInfo.constant, pipeInfo.w, pipeInfo.h);
+        context.drawImage(floorPipe, pipe[i].x, pipe[i].y + pipeInfo.constant);
 
-            // Move the pipes
-            pipe[i].x -= pipeInfo.speed; // range 1-3 (2)
+        pipe[i].x--;
 
-            // Loop the pipes around the canvas
-            if (pipe[i].x <= -pipeInfo.w) {
-                pipe[i].x = canvas.width - pipeInfo.w + pipeInfo.dis;
-                pipe[i].y = Math.floor(Math.random() * pipeInfo.h) - pipeInfo.h;
-            }
-
-            // If the bird hits a pipe
-            if (bird.x + bird.w >= pipe[i].x && bird.x <= pipe[i].x + pipeInfo.w && (bird.y <= pipe[i].y + pipeInfo.h || bird.y + bird.h >= pipe[i].y + pipeInfo.constant) || bird.y + bird.h >= groundInfo.y) {
-                environment.play = false;
-            }
-
-            // If the bird passes the pipes
-            if (bird.x + bird.w / 2 >= pipe[i].x){  
-                environment.score++;
-            }
+        // If this is set to a very large number, it will tank chrome... too bad!
+        if (pipe[i].x === 200) { // --------> 200 is classic flappy bird. Range around 0-300
+            pipe.push({  
+                x: canvas.width,
+                y: Math.floor(Math.random() * pipeInfo.h) - pipeInfo.h
+            });
         }
 
-        // Draw the ground
-        context.drawImage(ground, groundInfo.x, groundInfo.y);
+        // If the birth hits a pipe
+        if (bird.x + bird.w >= pipe[i].x && bird.x <= pipe[i].x + pipeInfo.w && (bird.y <= pipe[i].y + pipeInfo.h || bird.y + bird.h >= pipe[i].y + pipeInfo.constant) || bird.y + bird.h >= canvas.height) {
+            location.reload();
+        }
 
-        // Account for gravity on the bird and adjust the bird
-        bird.y += bird.speed;
-        bird.speed += 0.33; // Range: 0.2-0.45 (0.33)
-
-        requestAnimationFrame(loop);
+        // If the bird passes the pipes
+        if (pipe[i].x === 5){  
+            environment.score++;   
+        }
     }
-}
 
-function reset() {
-    bird.y = 200;
-    bird.speed = 0;
-    environment.score = 0;
-    initPipes();
+    // Account for gravity on the bird and adjust the bird
+    bird.y += bird.speed;
+    bird.speed += 0.1; // --------> 0.1 is about the right gravity for classic flappy bird. Range around 0.05-0.2
+
     requestAnimationFrame(loop);
-}
-
-function initPipes() {
-    for (var i = 0; i < pipeInfo.num; i++) {
-        pipe[i] = {
-            x: canvas.width + i * pipeInfo.dis,
-            y: Math.floor(Math.random() * pipeInfo.h) - pipeInfo.h
-        };
-    }
 }
 
 // Add action listeners
@@ -119,12 +96,8 @@ document.addEventListener("keydown", keyDownHandler, false);
 function keyDownHandler(e) {
     // space pressed
     if (e.key === " ") {
-        if (environment.play === true) {
-            moveUp();
-        }
-        else {
-            environment.play = true;
-            reset();
-        }
+        moveUp();
     }
 }
+
+requestAnimationFrame(loop);
